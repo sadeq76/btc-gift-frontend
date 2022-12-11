bd407a55788875ba6f3d866c20e085672f8a5f61
sadeq76 <sadeqshahmoradi76@gmail.com>
2022-12-12 01:05:49 +0330

    fine

--- src/pages/how-to-use/index.vue
+++ src/pages/how-to-use/index.vue
@@ -1,10 +1,26 @@
 <template>
-  <div dir="rtl" class="html-content max-w-1366 p-4" v-html="data"></div>
+  <div class="max-w-1366 p-4">
+    <h1 class="text-center flex flex-col">
+      <span class="text-primary text-sm mb-2"> نحوه کار با بی تی سی گیفت </span>
+      <span> شما در سه مرحله می توانید کیف خود را شارژ کنید </span>
+    </h1>
+    <div v-for="(part, index) in htu.mainRoad" :key="index">
+      <img :src="getImageAbsolutePath(part.image)" :alt="part.title" />
+      <h2>{{ part.title }}</h2>
+      <p>{{ part.description }}</p>
+    </div>
+  </div>
 </template>
 <script lang="ts" setup>
-const appConfig = useAppConfig();
-const content = ref<string>("");
-const { data } = useFetch("hello");
+import { default as htu } from "../../public/content/how-to-use.json";
+
+const glob = import.meta.glob("~/assets/images/how-to-use/*", {
+  eager: true,
+});
+
+const getImageAbsolutePath = (imageName: string): string => {
+  return glob[`/assets/images/how-to-use/${imageName}`]["default"];
+};
 </script>
 <style lang="scss" module="TheHTU">
 </style>
